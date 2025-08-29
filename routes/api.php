ececb3be6c888d2e64a2d7e0639e3cb30a4fcac2
Mohamed Sayed <mohamedsayed051213@gmail.com>
2025-08-29 20:33:27 +0300

    Made an end-point to get all tickets from DB

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
+
+Route::get('/tickets',[TicketController::class,'index']);
