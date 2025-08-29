78079bcaeac2c33f2b23258088a67f10299abbca
Mohamed Sayed <mohamedsayed051213@gmail.com>
2025-08-29 20:08:13 +0300

    Add Structure for login & register end-points

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
+use App\Http\Requests\ApiRegisterRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
@@ -9,7 +11,10 @@ class AuthController extends Controller
 {
     //
     use ApiResponses;
-    public function login() {
-        return $this->ok([],"Hello, Login",200);
+    public function login(ApiLoginRequest $request) {
+        return $this->ok([],$request->get('email'),200);
+    }
+    public function register(ApiRegisterRequest $request) {
+         return $this->ok([],$request->get('email'),200);
     }
 }
